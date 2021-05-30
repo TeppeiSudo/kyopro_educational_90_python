0614a908ac1884212d8bf75cbb2b5fba66f2816a
TeppeiSudo <sudo20.t.ab@gmail.com>
2021-05-30 12:17:43 +0900

    add and update 043*.py

--- sol/043_modified.py
+++ sol/043_modified.py
@@ -9,26 +9,28 @@ for h in range(H):
     maze[h] = str(input())
 
 inf = float("inf")
-dist = [[[inf, inf, inf, inf] for _ in range(W)] for _ in range(H)]
+dist = [inf] *4*W*H
 
 dx = [1, 0, -1, 0]
 dy = [0, 1, 0, -1]
 que = deque()
 for i in range(4):
-    dist[rs][cs][i] = 0
+    dist[rs*W+cs+i*W*H] = 0
     que.append((rs, cs, i))
 while que:
     u = que.pop()
     for i in range(4):
         tx = u[0] + dx[i]
         ty = u[1] + dy[i]
-        cost = dist[u[0]][u[1]][u[2]] + 1 if u[2] != i else dist[u[0]][u[1]][u[2]]
-        if (0 <= tx < H)and(0 <= ty < W)and(maze[tx][ty] == ".")and(dist[tx][ty][i] > cost):
-            dist[tx][ty][i] = cost
+        cost = dist[u[0]*W+u[1]+u[2]*W*H] + 1 if u[2] != i else dist[u[0]*W+u[1]+u[2]*W*H]
+        if (0 <= tx < H)and(0 <= ty < W)and(maze[tx][ty] == ".")and(dist[tx*W+ty+i*W*H] > cost):
+            dist[tx*W+ty+i*W*H] = cost
             if u[2] != i:
                 que.appendleft((tx, ty, i))
             else:
                 que.append((tx, ty, i))
 
-ans = min(dist[rt][ct])
-print(ans) # this solution raises TLE
+ans = inf
+for i in range(4):
+    ans = min(ans, dist[rt*W+ct+i*W*H])
+print(ans) # this solution is acceptable (AC)
